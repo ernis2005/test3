d942f8b84f74f7de2a38010128145436c7fd495a
ernis2005 <nhtfxnhtfx.9@gmail.com>
2023-02-24 02:37:48 +0600

    sdfs

--- pages/components/header/header.jsx
+++ pages/components/header/header.jsx
@@ -12,6 +12,34 @@ import { datadt } from "../../data";
 import { Badge, Checkbox } from "@mui/material";
 import { pink } from "@mui/material/colors";
 function Header() {
+  let datadt = [
+    {
+      id: 1,
+      name: " Lorem ipsum dolor sit.",
+      vendor: "L434-GH43",
+      price: 240,
+      priceTotal: 240,
+      quantity: 1,
+      img: "/img.png",
+    },{
+      id: 2,
+      name: " Lorem ipsum  sit.",
+      vendor: "L434-GH43",
+      price: 260,
+      priceTotal: 260,
+      quantity: 1,
+      img: "/img1.png",
+    }, {
+      id: 3,
+      name: " Lorem sit.",
+      vendor: "L434-GH43",
+      vendor: 1240,
+      quantity: 1,
+      img: "/img2.png",
+      price: 1200,
+      priceTotal: 1200,
+    },
+  ];
   const [data, setData] = useState(datadt);
   const [price, setPrice] = useState(1);
   const [mapindex, setmapindext] = useState();
